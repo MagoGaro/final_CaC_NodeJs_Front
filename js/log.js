78cba251af2cb9ed2c945abd266b44a2bbb98a33
Garo <94136201+MagoGaro@users.noreply.github.com>
2024-07-07 23:02:59 -0300

    Update log.js

--- js/log.js
+++ js/log.js
@@ -9,7 +9,7 @@ function convertDateFormat(string) {
     return info[2].substring(0,2) + '/' + info[1] + '/' + info[0] + ' ' + info2[0].substring(info2[0].length-n)+ ':'  + info2[1] + ':' + info2[2];
   }
 
-fetch('http://127.0.0.1:5000/log')
+fetch('https://5485732b-e059-4f80-96c1-8349a51548a1-00-3gwtqwkhsuvx5.riker.replit.dev/log')
 .then(res=> res.json())
 .then(data =>{
     console.log(data);
@@ -18,9 +18,9 @@ fetch('http://127.0.0.1:5000/log')
     let fecha = data[i]['fecha_hora'];
     
     logsito.innerHTML += `<tr>
-    <td scope="row">${data[i]['id_producto']}</td>
+    <td scope="row">${data[i]['nombre']}</td>
     <td>${data[i]['usuario']}</td>
-    <td>${data[i]['operacion']}</td>
+    <td>${data[i]['accion']}</td>
     <td>${convertDateFormat(fecha)}</td>
     </tr>
     
